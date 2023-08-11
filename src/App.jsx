44337d33be2b961eb36a35d0847e04b5bf281007
prasad <prasad@gamil.com>
2023-08-11 23:22:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import "./App.css"
+import Companies from "./components/Companies/Companies";
+import Recidencies from "./components/Recidencies/Recidencies";
+import Value from "./components/Value/Value";
+import Contact from "./components/Contact/Contact";
+import GetStarted from "./components/GetStarted/GetStarted";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return <div className="App">
+    <div>
+      <div className="white-gradient"/>
+      <Header/>
+      <Hero/>
+    </div>
+    <Companies/>
+    <Recidencies/>
+    <Value/>
+    <Contact/>
+    <GetStarted/>
+    <Footer/>
+  </div>
+}
+
+export default App;
